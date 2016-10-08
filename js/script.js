d57456da9c7a4d3cbabf1e695c0163ce37532e2a
Alexandre Pereira <alexandremmartinsp@gmail.com>
2016-10-08 02:14:09 +0100

    measure updated

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.window-width-size').text($(window).width() + 'px');
+  $('.window-height-size').text($(window).height() + 'px');
+});
+
+$(window).resize(function() {
+  var windowWidthSize = $(window).width();
+  var windowHeightSize = $(window).height();
+  $('.window-width-size').text(windowWidthSize + 'px');
+  $('.window-height-size').text(windowHeightSize + 'px');
+});

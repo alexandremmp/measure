f0d8189a05065e70f66b88c623a1495c14428b95
Alexandre Pereira <alexandremmartinsp@gmail.com>
2016-10-08 03:03:41 +0100

    layout upated

--- runtime/globals/js/script.js
+++ runtime/globals/js/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $('.window-width-size').text($(window).width() + 'px');
   $('.window-height-size').text($(window).height() + 'px');
+
+  $(".button").click(function(){
+    $(".measure").toggle();
+  });
 });
 
 $(window).resize(function() {

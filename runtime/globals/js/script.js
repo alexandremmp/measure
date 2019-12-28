de3ae440964ffe959ea349396397094935165fff
Alexandre Pereira <alexandremmartinsp@gmail.com>
2019-12-28 20:48:58 +0000

    repo updated

--- runtime/globals/js/script.js
+++ runtime/globals/js/script.js
@@ -1,21 +1,31 @@
-$(document).ready(function() {
-  $('.window-width-size').html(window.outerWidth + 'px');
-  $('.window-height-size').text($(window).height() + 'px');
+if (typeof (localStorage) === 'undefined') {
+  document.querySelector('result').innerHTML = 'Your browser does not support HTML5 localStorage. Try upgrading.';
+} else if (localStorage.getItem('block') != null) {
+  getVisible = localStorage.block;
+  $('.measure').addClass('block-is-invisible');
+}
 
-  $(".button").click(function(){
-    $(".measure").toggle();
-  });
+$(window).on('resize', () => {
+  $('.window-width-size').html(`${window.outerWidth}px`); // width of the page + scrollbar width
+  const windowHeightSize = $(window).height();
+  $('.window-height-size').text(`${windowHeightSize}px`);
 });
 
-(function($) {
+$(document).ready(() => {
+  $('.window-width-size').html(`${window.outerWidth}px`);
+  $('.window-height-size').text(`${$(window).height()}px`);
 
-  $(function() {
+  $('.btn-action').text($('.measure').hasClass('block-is-invisible') ? 'Show' : 'Hide');
 
-    $(window).on('resize', function() {
-      $('.window-width-size').html(window.outerWidth + 'px'); //width of the page + scrollbar width 
-      var windowHeightSize = $(window).height();
-      $('.window-height-size').text(windowHeightSize + 'px');
-    });
+  $('.btn-action').on('click', () => {
+    getVisible = localStorage.block;
+    $('.measure').toggleClass('block-is-invisible');
+    if ($('.measure').hasClass('block-is-invisible')) {
+      localStorage.setItem('block', 'block-is-invisible');
+      $('.btn-action').text('Show');
+    } else {
+      localStorage.removeItem('block');
+      $('.btn-action').text('Hide');
+    }
   });
-
-})(jQuery);
+});
